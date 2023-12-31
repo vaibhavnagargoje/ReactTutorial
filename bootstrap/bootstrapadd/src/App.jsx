b028162dbd49cbf938d22b0bc3bad9322cce8bf0
vaibhav <vaibhavnagargoje2000@gmail.com>
2023-12-31 23:32:50 +0530

    bootstrap add in react

---
+++ bootstrap/bootstrapadd/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>
+          hello word
+          <button type="button" class="btn btn-primary">
+            Primary
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Secondary
+          </button>
+          <button type="button" class="btn btn-success">
+            Success
+          </button>
+          <button type="button" class="btn btn-danger">
+            Danger
+          </button>
+          <button type="button" class="btn btn-warning">
+            Warning
+          </button>
+          <button type="button" class="btn btn-info">
+            Info
+          </button>
+          <button type="button" class="btn btn-light">
+            Light
+          </button>
+          <button type="button" class="btn btn-dark">
+            Dark
+          </button>
+          <button type="button" class="btn btn-link">
+            Link
+          </button>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
