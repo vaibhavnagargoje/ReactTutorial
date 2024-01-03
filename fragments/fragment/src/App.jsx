8a0e5c56f024af97f5012adbf9668970f5889826
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-03 17:15:20 +0530

    React fregment tuts

---
+++ fragments/fragment/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+
+
+      <div>Helthy food </div>
+      <ul className="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </React.Fragment>
+
+  );
+}
+
+export default App;
