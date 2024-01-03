da5fdc28bc2726e4cd01dd8bce0603bc3f91d279
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-03 17:14:30 +0530

    clock component changed

--- Clock/BharatClock/src/components/DateTime.jsx
+++ Clock/BharatClock/src/components/DateTime.jsx
@@ -1,6 +1,6 @@
 function DateTime(){
   let time= new Date();
-  return  <div className="container fw-bolder">
+  return  <div className="container fw-bolder lead">
         This is current timeis  {time.toLocaleTimeString()} and date is {time.toLocaleDateString()}.
       </div>
  
